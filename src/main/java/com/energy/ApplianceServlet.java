65beb1f4bf05df9c56cc78b2a96940c8583d4961
Nikolaev Andrei <An477393@gmail.com>
2025-10-02 19:53:22 +0300

    addStove

--- src/main/java/com/energy/ApplianceServlet.java
+++ src/main/java/com/energy/ApplianceServlet.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class ApplianceServlet extends HttpServlet {
 
     private static final String[] VALID_APPLIANCES = {
-        "lighting", "fridge", "washing_machine", "tv", "computer", "ac"
+        "stove", "lighting", "fridge", "washing_machine", "tv", "computer", "ac"
     };
 
     @Override
